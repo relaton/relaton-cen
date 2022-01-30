8583121e25fe055b9959c827dc081fd90aed099b
Andrei Kislichenko <android.2net@gmail.com>
2022-01-30 19:05:50 +0100

    update grammar

--- spec/relaton_cen/xml_parser_spec.rb
+++ spec/relaton_cen/xml_parser_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe RelatonCen::XMLParser do
   end
 
   it "create bibitem from YAML" do
-    hash = YAML.load_file 'spec/fixtures/bibdata.yaml'
+    hash = YAML.load_file "spec/fixtures/bibdata.yaml"
     bib = RelatonIsoBib::IsoBibliographicItem.from_hash hash
     expect(bib.to_hash).to eq hash
   end
